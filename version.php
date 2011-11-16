b1bca3fbae12e67c8a2137645568f94fdd630d7b
Matt Gibson <matt@inaura.net>
2011-11-16 13:05:05 +0000

    Added the rest of the upgrade stuf that didn't run for some users

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2011112505;
+$plugin->version = 2011112506;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2011070101; // 2.0
 $plugin->component = 'block_ajax_marking';
-$plugin->release = '2.1.4';
+$plugin->release = '2.1.5';
