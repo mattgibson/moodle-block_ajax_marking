98dd77ea318630f1504ef8a42d0b671afcabc9c3
Matt Gibson <matt@inaura.net>
2012-04-27 12:07:51 +0100

    Bumped version number

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2012042100;
+$plugin->version = 2012042600;
 $plugin->maturity = MATURITY_BETA;
 $plugin->requires = 2011070100; // 2.1
 $plugin->component = 'block_ajax_marking';
-$plugin->release = '2.1.5b8';
+$plugin->release = '2.1.5b9';
